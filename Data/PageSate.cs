60d28ceefb457f5ff31c80f9b004e9c2b6f9d0de
Vladimir Maiorov <vladimir.maiorov@stecpoint.ru>
2022-12-15 02:09:45 +0500

    load time

--- Data/PageSate.cs
+++ Data/PageSate.cs
@@ -2,6 +2,7 @@
 
 public class PageSate
 {
+  private DateTime? _startLoadTime;
   private string _message = string.Empty;
   public bool Loading { get; private set; }
 
@@ -19,19 +20,35 @@ public class PageSate
   {
     StartLoading();
   }
-  
+
   public void StartLoading()
   {
     Loading = true;
     Message = "Loading...";
+    _startLoadTime = DateTime.UtcNow;
   }
-  
+
   public void StopLoading()
   {
     Loading = false;
-    Message = "Done";
+    if (_startLoadTime != null)
+    {
+      var loadTime = DateTime.UtcNow - _startLoadTime;
+      if (loadTime.Value.TotalSeconds < 60)
+      {
+        Message = $"Loaded in {loadTime.Value.TotalSeconds:F1} seconds";
+      } else
+      {
+        Message = $"Loaded in {loadTime.Value.TotalMinutes:F2} minutes";
+      }
+
+    }
+    else
+    {
+      Message = "Done";
+    }
   }
-  
+
   public void SetMessage(string message)
   {
     Message = message;
